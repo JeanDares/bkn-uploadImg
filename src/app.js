061de607e5f7cc51ea47c0d129a5a53b0293e98a
Jean Borges <jeanborgesdares@gmail.com>
2025-02-11 18:42:43 -0300

    feat- add cors origin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 // src/app.js
-const express = require('express');
-const cors = require('cors');
-const imageRoutes = require('./routes/imageRoutes');
+const express = require("express");
+const cors = require("cors");
+const imageRoutes = require("./routes/imageRoutes");
 
 const app = express();
 
-app.use(cors());
+// Configuração correta do CORS
+app.use(cors({
+  origin: "https://frn-uploadimg.vercel.app/", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(imageRoutes);
 
